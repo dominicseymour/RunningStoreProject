6236b62d04b84ad93a805d72d26e73c5c0dc2852
Dom Seymour <djgseymour@gmail.com>
2020-05-12 11:14:22 +0100

    Created Users POJO class and added persistence.xml file

---
+++ src/com/runningstore/entity/Users.java
@@ -0,0 +1,42 @@
+package com.runningstore.entity;
+
+public class Users {
+
+	private Integer userId;
+	private String email;
+	private String fullName;
+	private String password;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
